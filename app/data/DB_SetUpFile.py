b58f39b5cebec52e9438bb8c79ee53901326e6e9
Vinitha Ravichandran <vinitha318@gmail.com>
2017-03-14 21:43:56 -0500

    db set up file initial commit

---
+++ app/data/DB_SetUpFile.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+sqlFile = "class.db"
+conn = sqlite3.connect(sqlFile)
+cur = conn.cursor()
+
+#print(cur.execute("SELECT * FROM Departments").fetchall())
+
+
+
+
+cur.execute("""INSERT INTO Courses VALUES ("The course that was inserted for the only purpose of testing","AAS462 IlliniClasses Course",  "Horrible DB design",3.6, 3.9, 4.8,1 )
+""")
+
+print(cur.execute("SELECT * FROM Courses").fetchall())
