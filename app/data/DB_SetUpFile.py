a0cc43c1ad5e6f3d50dd3aad75528260dc62ed44
Vinitha Ravichandran <vinitha318@gmail.com>
2017-03-14 21:47:18 -0500

    AVG functions - query for advance functionality

--- app/data/DB_SetUpFile.py
+++ app/data/DB_SetUpFile.py
@@ -11,5 +11,6 @@ cur = conn.cursor()
 
 cur.execute("""INSERT INTO Courses VALUES ("The course that was inserted for the only purpose of testing","AAS462 IlliniClasses Course",  "Horrible DB design",3.6, 3.9, 4.8,1 )
 """)
+cur.execute("""INSERT into Instructors()""")
 
 print(cur.execute("SELECT * FROM Courses").fetchall())
