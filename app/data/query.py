1b772c2676bf7e8e10515659f0255802896c55d7
Abhishek Verma <abhi@wirelessprv-10-195-248-98.near.illinois.edu>
2017-04-12 19:46:31 -0500

    db error

--- app/data/query.py
+++ app/data/query.py
@@ -2,7 +2,8 @@ import sqlite3
 
 class Query:
     def __init__(self):
-        self.conn  = sqlite3.connect("class.db", check_same_thread=False)
+        self.conn  = sqlite3.connect("app/data/class.db", check_same_thread=False) # if local
+        # self.conn = sqlite3.connect("class.db", check_same_thread=False) # if server
         self.cur = self.conn.cursor()
 
     # insert rating, post, review
