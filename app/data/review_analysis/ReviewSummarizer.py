5a3ece7b5e54acd7a4929c981920bd74f567876b
Abhishek Verma <abhi@Abhisheks-MacBook-Pro.local>
2017-04-19 13:50:04 -0500

    kjsdnf

---
+++ app/data/review_analysis/ReviewSummarizer.py
@@ -0,0 +1,32 @@
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+from gensim.summarization import summarize,keywords
+
+class ReviewSummarizer:
+
+    def __init__(self,reviews):
+        self.ratio=0.1
+        self.wordcount=50
+        self.reviews=reviews
+
+    def runSummaryAnalysis(self):
+
+
+    def getSummarizedText(self):
+        txt=' '.join(self.reviews)
+        return(summarize(text=txt,ratio=self.ratio,word_count=self.wordcount))
+    def getKeyWords(self):
+        txt = ' '.join(self.reviews)
+        return(keywords(text=txt, ratio=self.ratio))
+
+if __name__== "__main__":
+    with open("mockdata.txt") as fp:
+        reviews = fp.readlines()
+    sumsr= ReviewSummarizer(reviews)
+    print(sumsr.getKeyWords())
+    print(sumsr.getSummarizedText())
+
+
+
+
