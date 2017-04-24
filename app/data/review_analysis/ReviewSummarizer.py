b8c8daf4f42999f956001a0f52a4c712e7af71ee
Vinitha <vinitha318@gmail.com>
2017-04-24 13:52:13 -0500

    Review summarizer to give summarizes when unique token are less than 5

--- app/data/review_analysis/ReviewSummarizer.py
+++ app/data/review_analysis/ReviewSummarizer.py
@@ -7,20 +7,27 @@ class ReviewSummarizer:
 
     def __init__(self,reviews):
         self.ratio=0.1
-        self.wordcount=50
+        self.wordcount=20
         self.reviews=reviews
 
     def getSummarizedText(self):
         txt=' '.join(self.reviews)
-        print self.reviews
-
+        #print (self.reviews)
+        txt_input=' '.join(self.reviews)
         if len(self.reviews) == 0 or len(self.reviews) == 1:
             return " "
+        elif len(self.reviews) <5:
+            for i in range(10):
+                self.reviews.extend(self.reviews)
+            #print(len(self.reviews))
+            txt_input=' '.join(self.reviews)
         # if len(txt) < 500:
         #     return txt
         try:
-            summary = summarize(text=txt,ratio=self.ratio,word_count=self.wordcount)
-        except:
+            summary = summarize(text=txt_input,ratio=self.ratio,word_count=self.wordcount)
+            #print("sy")
+        except Exception as e:
+            #print(e)
             summary = txt
         return summary
 
@@ -28,12 +35,15 @@ class ReviewSummarizer:
         txt = ' '.join(self.reviews)
         return(keywords(text=txt, ratio=self.ratio))
 
-# if __name__== "__main__":
+'''if __name__== "__main__":
+    reviews = [' Test', 'This is a great course. I learnt a lot',
+                'Learnt a lot about databases and how they work in the background']
+    sumsr = ReviewSummarizer(reviews)
+    #print(sumsr.getKeyWords())
+    print(sumsr.getSummarizedText())
 #     with open("mockdata.txt") as fp:
-#         reviews = fp.readlines()
-#     sumsr= ReviewSummarizer(reviews)
-#     print(sumsr.getKeyWords())
-#     print(sumsr.getSummarizedText())
+#         reviews = fp.readlines()'''
+
 
 
 
