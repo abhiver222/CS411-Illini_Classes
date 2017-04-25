65c57a94dcf30b72f713c70ff43b3807cee870c2
Harrison Ding <harrisond2015@gmail.com>
2017-04-25 09:58:13 -0500

    Changed to server mode

--- app/data/query.py
+++ app/data/query.py
@@ -2,8 +2,8 @@ import sqlite3
 
 class Query:
     def __init__(self):
-        self.conn  = sqlite3.connect("app/data/class.db", check_same_thread=False) # if local
-        #self.conn = sqlite3.connect("class.db", check_same_thread=False) # if server
+        #self.conn  = sqlite3.connect("app/data/class.db", check_same_thread=False) # if local
+        self.conn = sqlite3.connect("class.db", check_same_thread=False) # if server
         self.cur = self.conn.cursor()
 
     # insert rating, post, review
